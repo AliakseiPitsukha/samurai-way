7d3eb4db2c031bc4b13bebb11f9103593bc54a6b
Aliaksei.Pitsukha <tonard89@gmail.com>
2022-10-19 20:06:09 +0300

    first APP changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technology/>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <>
+            <a>Profile</a>
+            <a>News</a>
+            <a>Messages</a>
+            <a>Music</a>
+
+        </>
+    )
+}
+
+const Technology = () => {
+    return (
+        <>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>ts</li>
+
+        </>
+    )
 }
 
 export default App;
