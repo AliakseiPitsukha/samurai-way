70bbc263c083a50e3b6dc9833be896bc9f2cbe0c
Aliaksei.Pitsukha <tonard89@gmail.com>
2022-10-25 17:33:41 +0300

    IT-Kamasutra, lesson 20, added NavLink and some stylisation

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import st from './SideBar.module.css'
 import {SideBarPropsType} from "../ComponentsType";
+import {NavLink} from "react-router-dom";
 
 export const SideBar = (props: SideBarPropsType) => {
     return (
         <nav className={st.sidebar}>
             <div className={st.sidebarLink}>
-                <a href='/profile'>Profile</a>
+                <NavLink to='/profile' activeClassName={st.active}>Profile</NavLink>
             </div>
             <div className={st.sidebarLink}>
-                <a href='/dialogs'>Messages</a>
+                <NavLink to='/dialogs' activeClassName={st.active}>Messages</NavLink>
             </div>
             <div className={st.sidebarLink}>
-                <a href='/news'>News</a>
+                <NavLink to='/news' activeClassName={st.active}>News</NavLink>
             </div>
             <div className={st.sidebarLink}>
-                <a href='/music'>Music</a>
+                <NavLink to='/music' activeClassName={st.active}>Music</NavLink>
             </div>
             <div className={st.sidebarLink}>
-                <a href='/settings'>Settings</a>
+                <NavLink to='/settings' activeClassName={st.active}>Settings</NavLink>
             </div>
         </nav>
     );
